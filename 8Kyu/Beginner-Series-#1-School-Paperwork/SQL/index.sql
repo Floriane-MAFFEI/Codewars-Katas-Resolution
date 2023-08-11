da491d2d36d3fb1a5c6db76519a483bf804efb45
Floriane-MAFFEI <floriane.oclock@gmail.com>
2023-08-11 11:53:41 +0200

    Fix - add of the instructions of the School Paperwork kata

--- 8Kyu/Beginner-Series-#1-School-Paperwork/SQL/index.sql
+++ 8Kyu/Beginner-Series-#1-School-Paperwork/SQL/index.sql
@@ -1,3 +1,11 @@
+-- Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+
+-- Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
+
+-- Example:
+-- n= 5, m=5: 25
+-- n=-5, m=5:  0
+
 -- # write your SQL statement here: you are given a table 'paperwork' 
 -- with columns 'id', n' and 'm', return a table with columns 'n', 'm' and your result in a column named 'res'.
 
