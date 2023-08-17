b618e8c128b1a5257512796b072a47e6b4961385
Floriane-MAFFEI <floriane.oclock@gmail.com>
2023-08-17 11:52:37 +0200

    Added resolved kata : 8Kyu/Function 1 - hello world (creative and classic solutions)

---
+++ 8Kyu/Function-1-hello-world/JS/index-classic-solution.js
@@ -0,0 +1,7 @@
+// Style Points
+// Sure, this is about as easy as it gets. But how clever can you be to create the most creative "hello world" you can think of? What is a "hello world" solution you would want to show your friends?
+
+// Classic Solution
+function greet() {
+    return "hello world!";
+    }
---
+++ 8Kyu/Function-1-hello-world/JS/index-creative-solution.js
@@ -0,0 +1,11 @@
+// Make a simple function called greet that returns the most-famous "hello world!".
+
+// Style Points
+// Sure, this is about as easy as it gets. But how clever can you be to create the most creative "hello world" you can think of? What is a "hello world" solution you would want to show your friends?
+
+// Creative Solution
+
+function greet() {
+    const x = ['H', 'E', 'L', 'L', 'O', ' ', 'W', 'O', 'R', 'L', 'D'];
+    return x.join('').toLowerCase() + '!';
+}
