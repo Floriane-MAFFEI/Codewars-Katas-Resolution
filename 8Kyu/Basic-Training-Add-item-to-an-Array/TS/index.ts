c11f7a37a7169595fb21005e219d9364f6af25ed
Floriane-MAFFEI <floriane.oclock@gmail.com>
2023-12-15 15:06:46 +0100

    Added resolved kata: 8Kyu\Basic Traing Add item to an Array

---
+++ 8Kyu/Basic-Training-Add-item-to-an-Array/TS/index.ts
@@ -0,0 +1,10 @@
+// Add the value "codewars" to the websites array.
+// After your code executes the websites array should == ["codewars"]
+
+// The websites array has already been defined for you using the following code:
+
+// export const websites: string[] = [];
+
+import { websites } from "./preloaded";
+websites.push("codewars");
+export { websites };
